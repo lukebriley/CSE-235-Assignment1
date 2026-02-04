33835f932bfd6222374726b9f944da32ba457549
default <l0rile06@louisville.edu>
2026-02-04 19:50:29 +0000

    added name.c

---
+++ name.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+#include<name_reverse.h>
+#include<name_length.h>
+
+void main() {
+	char* name = "Luke Riley";
+	printf(\n"%s", name\n);
+	
+	reverse_order(char*);
+	letter_count(char*);
+}
+
